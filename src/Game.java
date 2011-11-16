654d7b0062f7bce6015530429511f7092e4dca3d
justinbonne <justinbonne@c298d51d-2f8c-4401-84bb-0e5cdc279458>
2011-11-16 23:55:23 +0000

    Skeleton for implementing KeyListener added.
    Justin
    
    git-svn-id: https://subversion.assembla.com/svn/shark-bait/trunk@12 c298d51d-2f8c-4401-84bb-0e5cdc279458

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,7 @@
-public class Game implements MessageListener
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game implements MessageListener, KeyListener
 {
 	public Communication comm;
 	public Game()
@@ -15,4 +18,33 @@ public class Game implements MessageListener
 		Thread theThread = new Thread(game.comm = new Communication(game, "localhost", 7430));
 		theThread.start();
 	}
+    
+    public void keyTyped( KeyEvent pEvent )
+    {}
+	
+    public void keyPressed( KeyEvent pEvent )
+    {
+		if( pEvent.getKeyCode( ) == KeyEvent.VK_UP ){
+            
+		}
+		else if( pEvent.getKeyCode( ) == KeyEvent.VK_DOWN )
+        {
+            System.out.println("Down!");
+		}
+		else if( pEvent.getKeyCode() == KeyEvent.VK_LEFT )
+        { 
+            System.out.println("Left!");
+		}
+		else if( pEvent.getKeyCode() == KeyEvent.VK_RIGHT ) 
+        {
+            System.out.println("Right!");
+		}
+        else if( pEvent.getKeyCode() == KeyEvent.VK_TAB ) 
+        {
+            System.out.println("TAB!");
+		}
+	}
+	
+    public void keyReleased(KeyEvent e)
+    {}
 }
