e17558d67a36b227e1e45d652902822f867856a3
cardinal.blake0 <cardinal.blake0@c298d51d-2f8c-4401-84bb-0e5cdc279458>
2011-11-19 20:05:41 +0000

    git-svn-id: https://subversion.assembla.com/svn/shark-bait/trunk@16 c298d51d-2f8c-4401-84bb-0e5cdc279458

--- src/World.java
+++ src/World.java
@@ -1,21 +1,29 @@
-import java.awt.Point;
+package SharkBait;
+
 import java.util.ArrayList;
+import java.awt.Graphics;
 import java.awt.Polygon;
 
-public class World extends Sprite {
+public class World {
 
-	private double windDirection;
-	private double windSpeed;
-	private int time;
-	private int weatherState;
+	private double 	windDirection;
+	private double 	windSpeed;
+	private int 	time;
+	private int 	weatherState;
 	private ArrayList<Polygon> shore;
-	private int width;
-	private int height;
+	private int 	width;
+	private int 	height;
 	
 	
 	//	default constructor
 	public World ( ) {
-		
+		this.windDirection = 0;
+		this.windSpeed = 0;
+		this.time = 1;
+		this.weatherState = 0;
+		this.width = 5000;
+		this.height = 4000;
+		shore = new ArrayList<Polygon>();
 	}
 	
 	
@@ -28,7 +36,7 @@ public class World extends Sprite {
 	
 	
 	public void setWindSpeed ( int pWindSpeed ) {
-		this.windSpeed;
+		this.windSpeed = pWindSpeed;
 	}
 	public double getWindSpeed ( ) {
 		return this.windSpeed;
@@ -36,7 +44,7 @@ public class World extends Sprite {
 	
 	
 	public void setTime ( int pTime ) {
-		this.time;
+		this.time = pTime;
 	}
 	public int getTime ( ) {
 		return this.time;
@@ -67,17 +75,32 @@ public class World extends Sprite {
 	}
 		
 
-	public void addShore ( ) {
+	public void addShore ( int [] pX, int [] pY, int pNumPoints  ) {
+		Polygon mShore = null;
+		
+		try {
+			mShore = new Polygon (pX, pY, pNumPoints);
+		}
+		catch (NegativeArraySizeException e){
+			System.out.println( "Array size must be positive integer" );
+		}
+		catch (IndexOutOfBoundsException e){
+			System.out.println( "Array Index is out of bounds" );
+		}
+		catch (NullPointerException e){
+			System.out.println( "Array Null pointer exception" );
+		}
+		
+		if (mShore != null){
+			this.shore.add(mShore);
+		}
+	}
+		
+	public void draw (Graphics g ) {
 		
 	}
 	
-	
-	public update ( ) {
-		
-	}
-	
-	
-	public draw ( ) {
+	public void update (Graphics g ) {
 		
 	}
 	
