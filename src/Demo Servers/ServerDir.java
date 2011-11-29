4843d4598e5c10fe0f7d9cc4068fc96ff6b7d9b8
justinbonne <justinbonne@c298d51d-2f8c-4401-84bb-0e5cdc279458>
2011-11-29 07:55:35 +0000

    Scripted servers to showcase certain features during our presentation
    Justin
    
    git-svn-id: https://subversion.assembla.com/svn/shark-bait/trunk@109 c298d51d-2f8c-4401-84bb-0e5cdc279458

---
+++ src/Demo Servers/ServerDir.java
@@ -0,0 +1,124 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ServerDir
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+            ServerSocket server = new ServerSocket(5283);
+            System.out.println("Waiting");
+            Socket connection = server.accept();
+            System.out.println("Accepted");
+            DataOutputStream client_out = new DataOutputStream(connection.getOutputStream());
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+            client_out.writeBytes("registered:1;\n");
+            //client_out.writeBytes("shore:3:100:200:300:200:200:300;");	
+            client_out.writeBytes("shore:3:360:430:380:410:380:430;\n");
+            client_out.writeBytes("shore:5:420:410:430:390:440:410:440:430:420:430;\n");
+            client_out.writeBytes("shore:x;\n");
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+            client_out.writeBytes("start;\n");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            client_out.writeBytes("ship:1:0;\n");
+            client_out.writeBytes("shipState:1:400:450:0:0:3.0;\n");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:0:45:3.0;\n");
+            try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:0:90:3.0;\n");
+            try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:0:135:3.0;\n");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            client_out.writeBytes("shipState:1:400:450:0:180:3.0;\n");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:0:0:3.0;\n");
+            try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:0:-45:3.0;\n");
+            try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:0:-90:3.0;\n");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            client_out.writeBytes("shipState:1:400:450:0:-135:3.0;\n");
+            try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            client_out.writeBytes("shipState:1:400:450:0:-180:3.0;\n");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("gameover;");
+            client_out.close();
+            connection.close();
+		}
+		catch(IOException e)
+		{
+            
+		}
+	}
+}
---
+++ src/Demo Servers/ServerSpeed.java
@@ -0,0 +1,88 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ServerSpeed
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+            ServerSocket server = new ServerSocket(5283);
+            System.out.println("Waiting");
+            Socket connection = server.accept();
+            System.out.println("Accepted");
+            DataOutputStream client_out = new DataOutputStream(connection.getOutputStream());
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+            client_out.writeBytes("registered:1;\n");
+            //client_out.writeBytes("shore:3:100:200:300:200:200:300;");	
+            client_out.writeBytes("shore:3:360:430:380:410:380:430;\n");
+            client_out.writeBytes("shore:5:420:410:430:390:440:410:440:430:420:430;\n");
+            client_out.writeBytes("shore:x;\n");
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+            client_out.writeBytes("start;\n");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            client_out.writeBytes("ship:1:0;\n");
+            client_out.writeBytes("shipState:1:400:450:5:45:3.0;\n");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:15:45:3.0;\n");
+            try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:30:45:3.0;\n");
+            try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("shipState:1:400:450:60:45:3.0;\n");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			client_out.writeBytes("gameover;");
+            client_out.close();
+            connection.close();
+		}
+		catch(IOException e)
+		{
+
+		}
+	}
+}
