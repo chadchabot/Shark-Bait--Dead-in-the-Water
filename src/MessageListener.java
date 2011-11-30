a6f2eedc3f0708960b424001d634533c21eda220
chadchabot <chadchabot@c298d51d-2f8c-4401-84bb-0e5cdc279458>
2011-11-30 02:35:47 +0000

    MessageListener.java: So, yeah. Commenting abstract class? Not sure how that works, but there was no good resource on the web to answer that question.
    git-svn-id: https://subversion.assembla.com/svn/shark-bait/trunk@118 c298d51d-2f8c-4401-84bb-0e5cdc279458

--- src/MessageListener.java
+++ src/MessageListener.java
@@ -1,3 +1,18 @@
+/**
+ * @author      Bonne Justin jbonne@uoguelph.ca
+ * @author      Cardinal, Blake bcardina@uoguelph.ca
+ * @author      Chabot, Chad chabot@uoguelph.ca
+ * @version     0.9                 
+ * @since       2011-11-29
+ */
+
+
+/**
+ * An abstract class.
+ * Ensures that any class implementing MessageListener has
+ * the method handleMessage().
+ */
+
 public interface MessageListener 
 {
     public void handleMessage(Message message);
