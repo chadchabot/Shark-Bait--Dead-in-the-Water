20c79ea2fe15a2c18cec8b8a04f42d23a927acf5
justinbonne <justinbonne@c298d51d-2f8c-4401-84bb-0e5cdc279458>
2011-11-30 01:49:56 +0000

    SPIRTE: COMMMENNNTTSSSS
    Justin
    
    git-svn-id: https://subversion.assembla.com/svn/shark-bait/trunk@113 c298d51d-2f8c-4401-84bb-0e5cdc279458

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,50 +1,94 @@
-import java.util.HashMap;
-import java.awt.Point;
+/**
+ * @author      Bonne Justin jbonne@uoguelph.ca
+ * @author      Cardinal, Blake bcardina@uoguelph.ca
+ * @author      Chabot, Chad chabot@uoguelph.ca
+ * @version     0.9                 
+ * @since       2011-11-29
+ */
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.Point;
+
 import java.io.File;
 import java.io.IOException; 
+
+import java.util.HashMap;
+
 import javax.imageio.ImageIO;
 
-public class Sprite {
-        protected Point position;
-    protected String name;
-        protected int sWidth;
-        protected int sHeight;
-    protected int rotation;
-    protected String currentState = "default";
-        protected HashMap<String,BufferedImage> frames;
+public class Sprite 
+{
+    // details
+    protected String                        name;
+    protected Point                         position;
+    protected int                           sWidth = 0;
+    protected int                           sHeight = 0;
+    protected int                           rotation = 0;
     
-    public Sprite ( ) {
+    //used for animations
+    protected String                        currentState = "default";
+    protected HashMap<String,BufferedImage> frames;
+    
+    /**
+     * Class constructor intializes the name, frames hashmap and position to default values
+     */
+    public Sprite ( ) 
+    {
         this.name = "noname";
-                frames = new HashMap<String, BufferedImage>( );
-                this.position = new Point(0,0);
-        }
-    public Sprite (String pName ) {
+        this.frames = new HashMap<String, BufferedImage>( );
+        this.position = new Point(0,0);
+    }
+    /**
+     * Class constructor intializes the frames hashmap and position to default values
+     *
+     * @param pName name of the sprite
+     */
+    public Sprite ( String pName ) 
+    {
         this.name = pName;
         frames = new HashMap<String, BufferedImage>( );
         this.position = new Point(0,0);
     }
-    
+    /**
+     * Class constructor intializes the frames hashmap and position to default values
+     * Setups up the default frame image to specific image
+     *
+     * @param pName name of the sprite
+     * @param pFileName name of the image used for the default frame
+     */
     public Sprite ( String pName, String pFileName )
 	{
         frames = new HashMap<String, BufferedImage>( );
         this.position = new Point(0,0);
         this.name = pName;
-        //      load default
+        
+        // load default frame
         this.loadImage("default", pFileName);
-                //      get image dimensions
+        
+        // get image dimensions
         this.sWidth = frames.get("default").getWidth();
         this.sHeight = frames.get("default").getHeight();
-        }
-        
-        //      loads a particular frame (or frames?) of the image
-    public void loadImage ( String pName, String pFileName )
+    }
+    /**
+     * loads an image and adds to the frames hashmap using a secified key
+     *
+     * only looks in the images folder
+     * automatically appends .png to the filename so it is not needed
+     *
+     * @param pFrameName the desired key for the frame
+     * @param name of the image file
+     */
+    public void loadImage ( String pFrameName, String pFileName )
     {
         try
         {
-            frames.put( pName, ImageIO.read( new File("../images/"+ pFileName + ".png") ) );
-            if( pName.equals("default") )
+            frames.put( pFrameName, 
+                       ImageIO.read( new File("../images/"+ pFileName + ".png") ) );
+            
+            // if a new default frame is being loaded reset the dimensions to match
+            // the new image
+            if( pFrameName.equals("default") )
             {
                 this.sWidth = frames.get("default").getWidth();
                 this.sHeight = frames.get("default").getHeight(); 
@@ -52,24 +96,27 @@ public class Sprite {
         }
         catch ( IOException e )
         {
-            System.out.println("Can't load image: loadImage("+ pFileName +") ARGGGG");
+            System.out.println("Sprite: Can't load image: loadImage("+ pFileName +") ARGGGG");
         }
-        }
-        
-        //      change the index of the animation
-        public void update ( Graphics g )
+    }
+    /**
+     * usually updates the sprite based on some calculation
+     *
+     * extended in subclasses
+     */
+    public void update ( )
     {
 
-        }
-        public void draw ( Graphics g )
+    }
+    /**
+     * Draw the sprite to a graphics object
+     *
+     * @param g Graphics object that the sprite will be drawn on
+     */
+    public void draw ( Graphics g )
     {
         g.drawImage(this.frames.get(this.currentState), 
                     this.position.x, this.position.y, 
                     this.position.x + this.sWidth, this.position.y +this.sHeight, null);
-        }
-    
-    public static void main(String[] args )
-    {
-        Sprite image = new Sprite( "water");
     }
 }
