8bc0d61ab363e2695b24f8528d25c960a828a5f7
chadchabot <chadchabot@c298d51d-2f8c-4401-84bb-0e5cdc279458>
2011-11-30 02:16:41 +0000

    GUI.java: Javadoc compliant. Obviously.
    git-svn-id: https://subversion.assembla.com/svn/shark-bait/trunk@115 c298d51d-2f8c-4401-84bb-0e5cdc279458

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,11 @@
+/**
+ * @author      Bonne Justin jbonne@uoguelph.ca
+ * @author      Cardinal, Blake bcardina@uoguelph.ca
+ * @author      Chabot, Chad chabot@uoguelph.ca
+ * @version     0.9                 
+ * @since       2011-11-29
+ */
+
 
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -9,43 +17,57 @@ import java.util.HashMap;
 
 public class GUI extends Sprite
 {
+	
     public static final int		REFRESH_RATE = 60;
 	public static final int 	PIXELS_PER_METER = 1;
     public static final int     PLAYER_X_CENTER = 500;
     public static final int     PLAYER_Y_CENTER = 300;
 	
+	//	environmental measures
 	private int		windDirection;
 	private int		windSpeed;
-	private Point	windSockPosition;	//	position of windsock on screen
+	private Point	windSockPosition;
 	private int		imgWidth = 81;
 	
-	//	on-screen 'chrome'
+	//	fonts used to display ship health
 	private	static Font healthDisplayFont = new Font( "Courier", Font.BOLD, 20 );
 	private	static Font enemyListTitleFont = new Font( "Courier", Font.BOLD, 30 );
-	private double shipHealth		= 0.0;
-	private double shipHealthMAX	= 0.0;
 	
-	
-	//	when GUI() constructor is called, do we already have wind information?
+	/**
+     * Class constructor sets intialized GUI with default values
+     * Loads images and frames compass and targeting arrow
+     */
     public GUI( )
     {
-		this.windSockPosition = new Point( 923, 61 );
-        this.windDirection = 45;
-		this.windSpeed = 0;
+		this.windSockPosition	= new Point( 923, 61 );
+        this.windDirection		= 45;
+		this.windSpeed			= 0;
 		this.loadImage("default","compass_arrow");
         this.loadImage("target_arrow","target_arrow");        
     }
 
-	//	?	we need to have a list of ships -> to get the health of each 
-	//	?	wind direction, wind speed
+	
+	/**
+     * Update for GUI elements
+     *
+     * @param pWindDir the direction of the wind, from 0-359
+     * @param pWindSpeed the value of the wind speed in the world
+     */
     public void update ( int pWindDir, int pWindSpeed  ) 
     {
 		//	System.out.println( "updating the wind in GUI" );
-		
-        this.windDirection = pWindDir;
-        this.windSpeed   = pWindSpeed;
+        this.windDirection	= pWindDir;
+        this.windSpeed		= pWindSpeed;
     }
 	
+	/**
+     * Draws the parts of the HUD on-screen
+	 *
+     * @param g Graphics object that will be used to draw on-screen HUD elements
+     * @param pShipList HashMap of ships in the game world
+     * @param pPlayerID ID number of the player's ship
+	 * @param pTargetedShipID ID number of the currently targeted enemy ship
+     */
     public void draw ( Graphics g, HashMap<String, Ship> pShipList, int pPlayerID, int pTargetedShipID )
     {    	
 		Graphics2D g2D = (Graphics2D) g;
@@ -60,10 +82,12 @@ public class GUI extends Sprite
 		g2D.drawString( "Enemy ships:", 40, 45 );
 
 		g2D.setFont( healthDisplayFont );
+		//	for each ship in pShipList, draw their health in the appropriate area
 		for ( String key : pShipList.keySet() )
 		{
             hpBarFill = (int) (100 * pShipList.get(key).getHealth()/pShipList.get(key).getHealthMAX());
-            if ( pShipList.get(key).getShipID() != pPlayerID )
+            //	for all enemy ships, draw in the top-left of the screen
+			if ( pShipList.get(key).getShipID() != pPlayerID )
 			{
 				if ( pShipList.get(key).getShipID() != -1 &&
 					 pShipList.get(key).getShipID() == pTargetedShipID )
@@ -88,14 +112,14 @@ public class GUI extends Sprite
 				g2D.fillRect( 150, 63 + counter * rectOffset, hpBarFill, 20 );
 				g2D.setColor( Color.white );
 				g2D.drawRect( 150, 63 + counter * rectOffset, 100, 20 );
-//				System.out.println( shipHealth + " " + shipHealthMAX + " " + hpBarFill );			
 				counter++;
                 
+				//	handles the targeting arrow rotation and display
                 target_rot = getShipArrow(pShipList.get(key), pShipList.get(Integer.toString(pPlayerID)));
                 
                 if(target_rot > 0)
                 {
-                    g2D.rotate( (Math.toRadians(target_rot)),
+                    g2D.rotate( (Math.toRadians( target_rot )),
                                PLAYER_X_CENTER ,
                                PLAYER_Y_CENTER );
                     g2D.drawImage(this.frames.get("target_arrow"), 
@@ -107,6 +131,7 @@ public class GUI extends Sprite
                                PLAYER_Y_CENTER );
                 }
 			}
+			//	for the player's ship, draw in the bottom right
 			else
 			{
 				g2D.drawString( "SHIP HP" , 700, 635 );
@@ -133,18 +158,50 @@ public class GUI extends Sprite
 				   this.windSockPosition.x + this.imgWidth/2,
 				   this.windSockPosition.y + this.imgWidth/2 );		
     }
-    /*
+
+	
+	/*
+     *
      * Mutators
+     *
+     */
+
+	/*
+	 *
+     * Mutator for wind direction
+	 *
+     * @param pWindDirection The updated value for wind direction in the game world
      */
     public void setWindDirection ( int pWindDirection ) 
     {
 		this.windDirection = pWindDirection;
 	}
-
+	
+	/**
+     * Mutator for wind speed
+	 *
+     * @param pWindSpeed The updated value for wind speed in the game world
+     */
     public void setWindSpeed ( int pWindSpeed ) 
     {
 		this.windSpeed = pWindSpeed;
 	}
+	
+	
+	
+	/*
+     *
+     * Accessors
+     *
+     */
+	
+	/**
+     * Accessor for the angle of an enemy ship, relative to the player's ship
+     *
+	 * @param enemy A Ship that is one of the enemies in the game world
+	 * @param player The player's Ship
+     * @return The angle/orientation of the enemy ship relative to the player's ship
+     */
     private int getShipArrow(Ship enemy, Ship player)
     {
         int enemyHeading = enemy.getHeading();
